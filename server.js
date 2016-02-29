43f4ae0dc17340d210efd465358c2f798711d983
ftvbftvbq <ftvbftvbq@163.com>
2016-02-29 10:27:43 +0800

    add post todos

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: "准备购物",
-	completed: false
-},{
-	id: 2,
-	description: "准备吃午饭",
-	completed: false
-}
+var todos = [
+// {
+// 	id: 1,
+// 	description: "准备购物",
+// 	completed: false
+// },{
+// 	id: 2,
+// 	description: "准备吃午饭",
+// 	completed: false
+// }
 
-]
+];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('./', function(req, res){
 
@@ -44,6 +49,17 @@ app.get('/todos/:id', function(req, res){
 		}
 
 });
+
+//Post
+
+app.post('/todos', function(req, res){
+	var body = req.body;
+	 body.id =  todoNextId++;
+
+	 todos.push(body);
+	 res.json(body);
+	 
+	});
 
 app.listen(PORT, function(){
 	console.log('Express start at '+ PORT + '!');
