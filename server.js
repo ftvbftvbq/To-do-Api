62bd9ab6e6e08044ea5523afb670b2f1e9808409
ftvbftvbq <ftvbftvbq@163.com>
2016-03-23 13:58:05 +0800

    all is done\!

--- server.js
+++ server.js
@@ -258,11 +258,37 @@ app.post('/users', function (req, res) {
 //users/login
 app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function (user) {
-		res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
-	}, function() {
+		var token = user.generateToken('authentication');
+		userInstance = user;
+
+
+		return db.token.create({
+			token: token
+		});
+	}).then(function (tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function () {
 		res.status(401).send();
+	});	
+
+
+	// db.user.authenticate(body).then(function (user) {
+	// 	res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
+	// }, function() {
+	// 	res.status(401).send();
+	// });
+});
+
+
+//Delete /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function() {
+		res.status(204).send();
+	}).catch(function() {
+		res.status(500).send();
 	});
 });
 
